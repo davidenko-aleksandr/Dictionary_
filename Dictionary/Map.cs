7bbae724d8ce1c183edd2e360d49c44c5ea46216
Aleksandr Davidenko <alex1190119022@gmail.com>
2019-12-12 00:51:07 +0200

    Добавьте файлы проекта.

---
+++ Dictionary/Map.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class Map<TKey, TValue>
+    {
+        private List<Item<TKey, TValue>> _items = new List<Item<TKey, TValue>>();
+
+        public int Count => _items.Count;
+
+        public IReadOnlyList<TKey> Keys => (IReadOnlyList<TKey>)_items.Select(i => i.Key).ToList();
+        public void Add(Item<TKey, TValue> item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            if (_items.Any(i => i.Key.Equals(item.Key)))
+            {
+                throw new ArgumentException($"Словарь уже содержит значение с ключем {item.Key}.", nameof(item));
+            }
+            _items.Add(item);
+        }
+       
+        public void Add(TKey key, TValue value)
+        {
+            if (key==null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            if (_items.Any(i => i.Key.Equals(key)))
+            {
+                throw new ArgumentException($"Словарь уже содержит значение с ключем {key}.", nameof(key));
+            }
+            var item = new Item<TKey, TValue> { Key = key, Value = value };
+            _items.Add(item);
+        }
+        
+        public void Remove(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            var item = _items.SingleOrDefault(i => i.Key.Equals(key));
+            if (item!=null)
+            {
+                _items.Remove(item);
+            }
+        }
+        public void Update(TKey key, TValue newValue)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (newValue == null)
+            {
+                throw new ArgumentNullException(nameof(newValue));
+            }
+            
+             if (!_items.Any(i=>i.Key.Equals(key)))
+            {
+                throw new ArgumentException($"Словарь не содержит значение с ключем {key}.", nameof(key));
+            }
+            
+            var item = _items.SingleOrDefault(i => i.Key.Equals(key));
+            if (item!=null)
+            {
+                item.Value = newValue;
+            }
+        }
+        public TValue Get(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            var item = _items.SingleOrDefault(i => i.Key.Equals(key))??
+                throw new ArgumentException($"Словарь не содержит значение с ключем {key}.", nameof(key));
+            return item.Value;
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Map<int, string>();
+            map.Add(1, "Smith");
+            map.Add(2, "Deniro");
+            map.Add(3, "Sigal");
+            map.Add(new Item<int, string>(4, "Lungren"));
+            map.Add(new Item<int, string>(5, "Stallone"));
+
+            ShowMap(map, "Created map");
+
+            map.Update(5, "Kruz");
+            ShowMap(map, "Updated map");
+
+            map.Remove(1);
+            ShowMap(map, "Cleared map");
+
+        }
+
+        private static void ShowMap(Map<int, string> map, string title)
+        {
+            
+            Console.WriteLine($"{title}: ");
+
+            foreach (var key in map.Keys)
+            {
+                var value = map.Get(key);
+                Console.WriteLine($"{key} - {value}");
+            }
+        }
+    }
+}
